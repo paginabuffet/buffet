71f1f1d4436a6995cfeeddd5d75fe1c5c52241ff
paginabuffet <67081576+paginabuffet@users.noreply.github.com>
2020-07-03 23:19:46 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -25,6 +25,7 @@ const DATA_CACHE_NAME = 'data-cache-v1';
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
+  '/',
   '/index.html',
   '/scripts/app.js',
   '/scripts/install.js',
